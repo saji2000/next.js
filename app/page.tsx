4d4750cfa9f5b5f3213b9848a9087eb98020a920
saji2000 <sajaddaneshmand79@gmail.com>
2023-12-18 23:21:31 -0700

    created the data and clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Link from "next/link";
-import ProductCart from "./components/ProductCart";
 
 export default function Home() {
   return (
     <main>
       <p>Hello World!</p>
-      <Link href="./users">users</Link>
-      <ProductCart></ProductCart>
     </main>
   );
 }
