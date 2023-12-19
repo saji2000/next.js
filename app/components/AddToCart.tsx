c000123a4460b89a57ac701e8f43261cae4cecf9
saji2000 <sajaddaneshmand79@gmail.com>
2023-12-18 23:07:56 -0700

    Initial commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("Add to cart")}>Add to cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ app/components/ProductCart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+
+const ProductCart = () => {
+  return (
+    <div>
+      <AddToCart></AddToCart>
+    </div>
+  );
+};
+
+export default ProductCart;
